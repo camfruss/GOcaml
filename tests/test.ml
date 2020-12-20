46a5bbde32dde63ba7f1379ebc18f19156cc12c5
Banpreet Singh <bs757@cornell.edu>
2020-12-20 17:57:10 -0500

    Added Ko test

--- tests/test.ml
+++ tests/test.ml
@@ -192,10 +192,9 @@ let game_tests = [
     SelfCaptureException;
   test_raises2 "StoneAlreadyExists" (step error_test) (Some (0, 1)) 0 
     StoneAlreadyExistsException;
+  test_raises2 "KoException" (step ko_game) (Some (5, 3)) 0 KoException;
 
   (* UNDO *)
-
-  (* SCORE *)
 ]
 
 let suite =
